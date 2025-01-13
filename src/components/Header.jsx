30e7cf381bdd6d026b14abf7abaad35068d293b5
JvDev2003 <jv.developer2003@gmail.com>
2025-01-13 12:35:50 -0300

    Adições CSS
    - Adicionado media querys para responsividade
    - Adicionado Menu na versão mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import styles from './Header.module.css'
+import { useState } from 'react'
 
 const Header = () => {
+
+  const [menuMobile, setMenuMobile] = useState(false)
+
+  const handleChange = () => {
+    setMenuMobile(!menuMobile)
+  }
+
   return (
     <header className={styles.menu}>
         <span>Portifólio</span>
-        <ul>
+        <ul className={styles.desktop}>
             <li><a href="#inicio">Inicio</a></li>
             <li><a href="#projetos">Projetos</a></li>
             <li><a href="#experiencias">Experiências</a></li>
@@ -12,6 +20,25 @@ const Header = () => {
             <li><a href="#certificacoes">Certificados</a></li>
             <li><a href="#conquistas">Conquistas</a></li>
         </ul>
+        <img 
+          src="https://img.icons8.com/?size=32&id=S5biqohaDgd1&format=png&color=FFFFFF" 
+          alt="botão abrir menu"
+          onClick={handleChange}
+        />
+        {
+          menuMobile && (
+            <div className={styles.mobile}>
+              <ul>
+              <li onClick={handleChange}><a href="#inicio">Inicio</a></li>
+                <li onClick={handleChange}><a href="#projetos">Projetos</a></li>
+                <li onClick={handleChange}><a href="#experiencias">Experiências</a></li>
+                <li onClick={handleChange}><a href="#formacoes">Formações</a></li>
+                <li onClick={handleChange}><a href="#certificacoes">Certificados</a></li>
+                <li onClick={handleChange}><a href="#conquistas">Conquistas</a></li>
+              </ul>
+            </div>
+          )
+        }
     </header>
   )
 }
